602efbc7f3ca29e07cec2f67a1c22a1ac96e3420
Aaron Santiago <me@aaron.work>
2024-09-01 21:38:36 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "../app.css";
+  import "$lib/fluid"
+  import { onMount } from "svelte";
+
+
+  onMount(() => {
+    window.setupFluid();
+  });
+</script>
+
+<div id="fluid-wrapper" class="absolute w-[110vw] h-[110vh] overflow-hidden top-[-5vh] left-[-5vw]">
+  <div class="absolute w-screen h-screen overflow-hidden top-[5vh] left-[5vw]">
+    <slot />
+  </div>
+</div>

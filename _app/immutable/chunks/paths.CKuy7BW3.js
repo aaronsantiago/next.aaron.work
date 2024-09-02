6bcfa81be184cdf2565dd89cb5f6c8cf5e59da55
Aaron Santiago <me@aaron.work>
2024-09-02 02:26:00 -0400

    Updates

--- _app/immutable/chunks/paths.CKuy7BW3.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1n0sxeg)==null?void 0:s.base)??"/next.aaron.work";var e;const t=((e=globalThis.__sveltekit_1n0sxeg)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/chunks/paths.CZ91onuK.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_bjl6be)==null?void 0:s.base)??"/next.aaron.work";var e;const t=((e=globalThis.__sveltekit_bjl6be)==null?void 0:e.assets)??a;export{t as a,a as b};

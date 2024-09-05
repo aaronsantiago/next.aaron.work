91355461e18a3abd265ace211e6f28b97fd6b1b4
Aaron Santiago <me@aaron.work>
2024-09-05 00:23:43 -0400

    update project page layouts

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -18,22 +18,28 @@
 
 </script>
 
-<div class="w-full grid grid-cols-2 grid-flow-dense">
+<div class="w-full grid grid-cols-2 grid-flow-dense gap-6 p-6">
 	<div 
 	in:fly|global={{ x: -100, duration: 400, delay:400, easing: quintOut }}
 	out:fly|global={{ x: -100, duration: 200, delay:100, easing: quintIn }}
 	class={clsx(
 		"text-4xl font-rbk",
-		`opacity-75 bg-[#fffB] p-10 m-4`,
-		'rounded-3xl drop-shadow-md',
+		`opacity-75 bg-[#fffB] p-10`,
+		'rounded-3xl drop-shadow-md max-h-[40dvw] overflow-y-scroll',
 		// "w-full"
 	)}><svelte:component this={data.content} /></div>
 	<!-- <div class=""
 		in:fly|global={{ x: 100, duration: 400, delay:600, easing: quintOut }}
 		out:fly|global={{ x: 100, duration: 200, easing: quintIn }}> -->
 		{#each data.images as image}
-			<div class="w-full h-full">
-				<img src={base + image} alt="" class="object-cover" />
+			<div class={clsx(`w-full h-full min-h-[40dvw]`,
+			`opacity-75 hover:opacity-100 transition-opacity relative`,
+			'rounded-3xl',
+			'drop-shadow-md'
+		)}
+		style={`background-image: url(${base + image}); background-position: center; background-size:cover;`}
+			>
+				<!-- <img src={base + image} alt="" class="object-cover" /> -->
 			</div>
 		{/each}
 	<!-- </div> -->
